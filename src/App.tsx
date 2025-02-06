8fc57932c0c95e5af582e72713cdeeb2e8708c46
agungtrilek <agungtrileksono1@gmail.com>
2025-02-06 19:41:29 +0700

    diubah menjadi atomic design

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,13 @@
 import React from "react";
-import Login from "./page/Login";
+import Login from "./page/llogin";
 import Dashboard from "./page/Dashboard";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Register from "./page/Register";
-import ProtectedRoute from "./protect/ProtectedRoute";
+import ProtectedRoute from "./components/layout/protect/ProtectedRoute";
+import Routers from "./route/Routers";
 
 const App: React.FC = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-    </Router>
-  );
+  return <Routers />;
 };
 
 export default App;
